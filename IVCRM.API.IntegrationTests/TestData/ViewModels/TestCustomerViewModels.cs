d56a9c104c4403a82c8feae3c31cff2c10ec9491
vadim <vadim.minutko@innowise-group.com>
2022-10-31 12:52:34 +0300

    implement api integration tests

--- IVCRM.API.IntegrationTests/TestData/ViewModels/TestCustomerViewModels.cs
+++ IVCRM.API.IntegrationTests/TestData/ViewModels/TestCustomerViewModels.cs
@@ -12,22 +12,29 @@ namespace IVCRM.API.IntegrationTests.TestData.ViewModels
             PhoneNumber = "+1234567",
         };
 
+        internal static CustomerViewModel UpdatedCustomerViewModel => new()
+        {
+            Id = 1,
+            FullName = "UpdatedFirstName UpdatedLastName",
+            PhoneNumber = "+1234567",
+        };
+
         internal static List<CustomerViewModel> CustomerViewModelCollection => new()
         {
             new ()
             {
                 FullName = "FirstName1 LastName1",
-                PhoneNumber = "1234567",
+                PhoneNumber = "+1234567",
             },
             new ()
             {
                 FullName = "FirstName2 LastName2",
-                PhoneNumber = "2234567",
+                PhoneNumber = "+2234567",
             },
             new ()
             {
                 FullName = "FirstName3 LastName3",
-                PhoneNumber = "3234567",
+                PhoneNumber = "+3234567",
             },
         };
 
@@ -35,6 +42,13 @@ namespace IVCRM.API.IntegrationTests.TestData.ViewModels
         {
             FirstName = "FirstName",
             LastName = "LastName",
+            PhoneNumber = "+1234567",
+        };
+
+        internal static ChangeCustomerViewModel UpdatedChangeCustomerViewModel => new()
+        {
+            FirstName = "UpdatedFirstName",
+            LastName = "UpdatedLastName",
             PhoneNumber = "+1234567",
         };
     }
