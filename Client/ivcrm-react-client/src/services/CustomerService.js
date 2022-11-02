1e08154138a86498ea4888780ec32782b9e901ab
vadim <genresal@yandex.ru>
2022-11-02 11:34:59 +0300

    add customer service

---
+++ Client/ivcrm-react-client/src/services/CustomerService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default class PostService {
+    static async getById(id) {
+        const response = await axios.get('https://localhost:7159/api/customer/' + id)
+        return response;
+    }
+}
