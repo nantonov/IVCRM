eeba4b39fc375ac820f0f95216edf0c871fbe89f
vadim <genresal@yandex.ru>
2022-11-02 14:33:03 +0300

    finish modal

--- Client/ivcrm-react-client/src/App.jsx
+++ Client/ivcrm-react-client/src/App.jsx
@@ -8,6 +8,8 @@ import MyInput from "./components/UI/input/MyInput";
 import MySelect from "./components/UI/select/MySelect";
 
 import './styles/App.css';
+import MyModal from "./components/UI/modal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,8 +17,8 @@ function App() {
     {id: 2, title: 'Javascript2', body: 'Description2'},
     {id: 3, title: 'Javascript3', body: 'Description1'},
   ])
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
   {/* 
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
@@ -25,39 +27,39 @@ function App() {
 {/* мемоификация, кеширование другими словами*/}
 const sortedPosts = useMemo(() => {
   console.log('scscscsc')
-  if(selectedSort){
-    return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+  if(filter.sort){
+    return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
   }
   return posts;
-}, [selectedSort, posts])
+}, [filter.sort, posts])
 
 const sortedAndSearchedPosts = useMemo(() => {
-  return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-}, [searchQuery, sortedPosts])
+  return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+}, [filter.query, sortedPosts])
 
 const createPost = (newPost) => {
   setPosts([...posts, newPost])
+  setModal(false)
 }
 
 const removePost = (post) => {
   setPosts(posts.filter(x => x.id !== post.id))
 }
 
-const sortPosts = (sort) => {
-  setSelectedSort(sort);
-}
-
   return (
     <div className="App">
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create
+      </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost}/>
-        <hr style={{margin: '15px 0'}}/>
-        <PostFilter/>
+        </MyModal>
 
-        {sortedAndSearchedPosts.length !== 0
-          ? <PostList remove={removePost} posts = {sortedAndSearchedPosts} title="LIST!!!"/>
-          : <h1 style={{textAlign: 'center'}}>NO DATA</h1>
-        }
-        
+        <hr style={{margin: '15px 0'}}/>
+        <PostFilter filter={filter} setFilter={setFilter}/>
+
+        <PostList remove={removePost} posts = {sortedAndSearchedPosts} title="LIST!!!"/>
+
       <Counter/>
       <Counter/>
     </div>
--- Client/ivcrm-react-client/src/components/PostFilter.jsx
+++ Client/ivcrm-react-client/src/components/PostFilter.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import MyInput from "./UI/button/MyInput";
-import MySelect from "./UI/button/MySelect";
+import MyInput from "./UI/input/MyInput";
+import MySelect from "./UI/select/MySelect";
 
-const PostFilter = (props) => {
+const PostFilter = ({filter, setFilter}) => {
     return (
         <div>
           <MyInput
-            value={searchQuery}
-            onChange={e => setSearchQuery(e.target.value)}
+            value={filter.query}
+            onChange={e => setFilter({...filter, query: e.target.value})}
             placeholder="Search..."
             />
           <MySelect
-            value={selectedSort}
-            onChange={sortPosts}
+            value={filter.sort}
+            onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
             defaultValue="Sort"
             options={[
               {value: 'title', name: 'By title'},
