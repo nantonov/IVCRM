1e0a31d98d59e255b47910cad73d985932e12b30
vadim <genresal@yandex.ru>
2022-11-07 22:36:34 +0300

    move to another folder

--- Services/IVCRM.BLL.UnitTests/Usings.cs
+++
@@ -1,4 +0,0 @@
-global using AutoMapper;
-global using FluentAssertions;
-global using IVCRM.BLL.UnitTests.TestData.Models;
-global using Xunit;
--- Services/IVCRM.DAL.IntegrationTests/Usings.cs
+++
@@ -1,3 +0,0 @@
-global using FluentAssertions;
-global using IVCRM.DAL.IntegrationTests.Infrastructure;
-global using Xunit;
