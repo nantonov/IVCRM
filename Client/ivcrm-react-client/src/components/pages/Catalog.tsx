f13d38353ed2f713e0bca21d7f587135fd673857
vadim <genresal@yandex.ru>
2022-11-23 11:41:05 +0300

    add tree component

--- Client/ivcrm-react-client/src/components/pages/Catalog.tsx
+++ Client/ivcrm-react-client/src/components/pages/Catalog.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ProductCategoryService from "../../services/ProductCategoryService";
-import TreeView from '@mui/lab/TreeView';
 import { IProductCategory } from '../../models/IProductCategory';
-import TreeItem from '@mui/lab/TreeItem/TreeItem';
-
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import CategoriesTree from '../categories/CategoriesTree';
+import { Grid } from '@mui/material';
 
 function Catalog() {
   const [categories, setCategories] = useState<Array<IProductCategory>>([])
@@ -19,21 +16,15 @@ function Catalog() {
     setCategories(categories)
   }
 
-  const renderTree = (tree: IProductCategory) => {
-    return <TreeItem key={tree.id} nodeId={tree.id.toString()} label={tree.id}>
-            {tree.childCategories && tree.childCategories.map(renderTree)}
-         </TreeItem>
-  } 
-
   return (
-<TreeView
-  aria-label="file system navigator"
-  defaultCollapseIcon={<ExpandMoreIcon />}
-  defaultExpandIcon={<ChevronRightIcon />}
-  sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
->
-    {renderTree(categories[0])}
-    </TreeView>
+    <Grid direction='row' container spacing={1}>
+      <Grid container item sm={3}>
+        <CategoriesTree categories={categories}/>
+      </Grid>
+      <Grid container item sm={9}>
+        Some catalog...
+      </Grid>
+    </Grid>
   );
 }
 
